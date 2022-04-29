25fdcac9490e7f89fcb40e357f4f3c817964abae
Rachel <Rachel.Heneault@gmail.com>
2022-04-29 07:35:52 -0400

    updated the delete function

--- src/components/App.js
+++ src/components/App.js
@@ -6,11 +6,16 @@ import FeedbackData from '../data/FeebackData'
 //need to add the data in
 const App = () => {
   const [feedback, setFeedback] = useState(FeedbackData)
+  const deleteFeedback = id => {
+    if (window.confirm('Are you sure you want to delete?')) {
+      setFeedback(feedback.filter(item => item.id !== id))
+    }
+  }
   return (
     <div>
       <Header></Header>
       <div className='container'>
-        <FeedbackList feedback={feedback}></FeedbackList>
+        <FeedbackList feedback={feedback} handleDelete={deleteFeedback}></FeedbackList>
       </div>
     </div>
   )
