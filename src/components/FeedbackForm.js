dd4fedb983aed7cfdfee8e6a063337d9f08fd3d0
Rachel <Rachel.Heneault@gmail.com>
2022-05-04 18:42:57 -0400

    working on the rating component

--- src/components/FeedbackForm.js
+++ src/components/FeedbackForm.js
@@ -1,22 +1,39 @@
 import { useState } from 'react'
 import Card from './shared/Card'
 import Button from './shared/Button'
+import RatingSelect from './RatingSelect'
 
 const FeedbackForm = () => {
   const [text, setText] = useState('')
+  const [rating, setRating] = useState(10)
+  const [btnDisabled, setBtnDisabled] = useState(true)
+  const [message, setMessage] = useState('')
   const handleTextChange = e => {
+    if (text === '') {
+      setBtnDisabled(true)
+      setMessage(null)
+    } else if (text !== '' && text.trim().length <= 10) {
+      setMessage('Text must be at least 10 characters')
+      setBtnDisabled(true)
+    } else {
+      setMessage(null)
+      setBtnDisabled(false)
+    }
     setText(e.target.value)
   }
   return (
     <Card>
       <form>
         <h2>How would you rate our service?</h2>
-        {/* rating component */}
+        <RatingSelect />
         <div className='input-group'>
           <input onChange={handleTextChange} type='text' value={text} placeholder='Write a review' />
 
-          <Button type='submit'>Send</Button>
+          <Button type='submit' isDisabled={btnDisabled}>
+            Send
+          </Button>
         </div>
+        {message && <div className='message'>{message}</div>}
       </form>
     </Card>
   )
---
+++ src/components/RatingSelect.js
@@ -0,0 +1,55 @@
+import { useState } from 'react'
+import PropTypes from 'prop-types'
+
+const RatingSelect = () => {
+  const [selected, setSelected] = useState(10)
+  const handleChange = e => {
+    setSelected(+e.currentTarget.value)
+  }
+  return (
+    <ul className='rating'>
+      <li>
+        <input type='radio' name='rating' value='1' id='num1' onChange={handleChange} checked={selected === 1} />
+        <label htmlFor='num1'>1</label>
+      </li>
+      <li>
+        <input type='radio' name='rating' value='2' id='num2' onChange={handleChange} checked={selected === 2} />
+        <label htmlFor='num2'>2</label>
+      </li>
+      <li>
+        <input type='radio' name='rating' value='3' id='num3' onChange={handleChange} checked={selected === 3} />
+        <label htmlFor='num3'>3</label>
+      </li>
+      <li>
+        <input type='radio' name='rating' value='4' id='num4' onChange={handleChange} checked={selected === 4} />
+        <label htmlFor='num4'>4</label>
+      </li>
+      <li>
+        <input type='radio' name='rating' value='5' id='num5' onChange={handleChange} checked={selected === 5} />
+        <label htmlFor='num5'>5</label>
+      </li>
+      <li>
+        <input type='radio' name='rating' value='6' id='num6' onChange={handleChange} checked={selected === 6} />
+        <label htmlFor='num6'>6</label>
+      </li>
+      <li>
+        <input type='radio' name='rating' value='7' id='num7' onChange={handleChange} checked={selected === 7} />
+        <label htmlFor='num7'>7</label>
+      </li>
+      <li>
+        <input type='radio' name='rating' value='8' id='num8' onChange={handleChange} checked={selected === 8} />
+        <label htmlFor='num8'>8</label>
+      </li>
+      <li>
+        <input type='radio' name='rating' value='9' id='num9' onChange={handleChange} checked={selected === 9} />
+        <label htmlFor='num9'>9</label>
+      </li>
+      <li>
+        <input type='radio' name='rating' value='10' id='num10' onChange={handleChange} checked={selected === 10} />
+        <label htmlFor='num10'>10</label>
+      </li>
+    </ul>
+  )
+}
+
+export default RatingSelect
